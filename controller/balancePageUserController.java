ff7c38b62328877b9babaf8ca767675ff409ebe6
Andhikawahyuadhyaksa <148024887+Andhikawahyuadhyaksa@users.noreply.github.com>
2024-11-30 00:00:06 +0700

    Add files via upload

---
+++ controller/balancePageUserController.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package view;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+/**
+ * FXML Controller class
+ *
+ * @author Andhi
+ */
+public class balancePageUserController implements Initializable {
+
+    @FXML
+    private TextField totalSaldoUser_fill;
+    @FXML
+    private Button topUpSaldoUser_fill;
+    @FXML
+    private TextArea sewaAlatUser_fill;
+    @FXML
+    private TextArea beliPupukUser_fill;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
