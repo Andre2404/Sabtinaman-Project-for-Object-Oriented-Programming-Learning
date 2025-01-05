65fc7c07787dcf0d9c4f60dceb74d7a57c6d2ed4
Andre2404 <100183447+Andre2404@users.noreply.github.com>
2025-01-05 19:51:57 +0700

    Add files via upload

--- module-info.java
+++ module-info.java
@@ -6,8 +6,11 @@ module Main {
     requires java.base;
     requires javafx.base;
     requires javafx.graphics;
+    requires java.desktop;
     opens Main to javafx.fxml;
     exports Main;
     opens Controller to javafx.fxml;
     exports Controller;
+    opens model to javafx.base;
+    exports model;
 }
